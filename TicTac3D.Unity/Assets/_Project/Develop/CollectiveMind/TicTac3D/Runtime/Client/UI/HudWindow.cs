b321f2ea26594bcc5c6ccf5fe4c148bd6dd99d97
Subbota Mikhail <mihail.subbota@inbox.ru>
2025-03-22 02:29:26 +0300

    feat: Add settings button to hud

--- TicTac3D.Unity/Assets/_Project/Develop/CollectiveMind/TicTac3D/Runtime/Client/UI/HudWindow.cs
+++ TicTac3D.Unity/Assets/_Project/Develop/CollectiveMind/TicTac3D/Runtime/Client/UI/HudWindow.cs
@@ -1,8 +1,33 @@
-﻿using CollectiveMind.TicTac3D.Runtime.Client.WindowManagement;
+﻿using CollectiveMind.TicTac3D.Runtime.Client.UI.Settings;
+using CollectiveMind.TicTac3D.Runtime.Client.WindowManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 namespace CollectiveMind.TicTac3D.Runtime.Client.UI
 {
   public class HudWindow : BaseWindow
   {
+    [SerializeField]
+    private Button _settingsButton;
+
+    private IWindowManager _windowManager;
+
+    [Inject]
+    public void Construct(IWindowManager windowManager)
+    {
+      _windowManager = windowManager;
+      _settingsButton.AddListener(OpenPauseWindow);
+    }
+
+    private void OpenPauseWindow()
+    {
+      _windowManager.OpenWindow<SettingsWindow>();
+    }
+
+    private void OnDestroy()
+    {
+      _settingsButton.RemoveListener(OpenPauseWindow);
+    }
   }
 }

338330495f474b6254e4f1643ad2e09a925ad43a
Subbota Mikhail <mihail.subbota@inbox.ru>
2025-03-25 00:19:50 +0300

    feat: Design lobby settings window

--- TicTac3D.Unity/Assets/_Project/Develop/CollectiveMind/TicTac3D/Runtime/Client/UI/LobbySettings/LobbySettingsWindow.cs
+++ TicTac3D.Unity/Assets/_Project/Develop/CollectiveMind/TicTac3D/Runtime/Client/UI/LobbySettings/LobbySettingsWindow.cs
@@ -1,8 +1,10 @@
-﻿using CollectiveMind.TicTac3D.Runtime.Client.Gameplay;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CollectiveMind.TicTac3D.Runtime.Client.Gameplay;
 using CollectiveMind.TicTac3D.Runtime.Client.WindowManagement;
+using CollectiveMind.TicTac3D.Runtime.Shared.AssetManagement;
 using CollectiveMind.TicTac3D.Runtime.Shared.Gameplay;
 using CollectiveMind.TicTac3D.Runtime.Shared.Gameplay.Rules;
-using CollectiveMind.TicTac3D.Runtime.Shared.Gameplay.Shape;
 using CollectiveMind.TicTac3D.Runtime.Shared.Network;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -19,46 +21,60 @@ namespace CollectiveMind.TicTac3D.Runtime.Client.UI.LobbySettings
     [SerializeField]
     private Button _backButton;
 
+    [SerializeField]
+    private Sprite _activeButtonSprite;
+
+    [SerializeField]
+    private Sprite _inactiveButtonSprite;
+
     private GameRulesProvider _rulesProvider;
     private IWindowManager _windowManager;
     private IRpcProvider _rpcProvider;
+    private IConfigLoader _configLoader;
+    private GameConfig _config;
+    private List<RuleButton> _ruleButtons;
 
     private GameRules Rules => _rulesProvider.Rules;
 
     [Inject]
-    public void Construct(GameRulesProvider gameRulesProvider, IWindowManager windowManager, IRpcProvider rpcProvider)
+    public void Construct(GameRulesProvider gameRulesProvider,
+      IWindowManager windowManager,
+      IRpcProvider rpcProvider,
+      IConfigLoader configLoader)
     {
       _rulesProvider = gameRulesProvider;
       _windowManager = windowManager;
       _rpcProvider = rpcProvider;
+      _configLoader = configLoader;
+      _config = configLoader.LoadConfig<GameConfig>();
+
+      _ruleButtons = GetComponentsInChildren<RuleButton>(true).ToList();
 
       _searchGameButton.AddListener(SearchGame);
       _backButton.AddListener(CloseWindow);
     }
 
-    public void SelectShape(ShapeType shapeType)
+    private void Start()
     {
-      Rules.Data.DesiredShape = shapeType;
+      Rules.Data = _config.DefaultRules;
+      
+      foreach (RuleButton button in _ruleButtons)
+      {
+        button.SetSprite(button.IsEqualRule(Rules) ? _activeButtonSprite : _inactiveButtonSprite);
+      }
     }
 
-    public void SelectBotMoveCount(int count)
+    public void SetRule<T>(RuleButton sender, T value)
     {
-      Rules.Data.BotMoveCount = count;
-    }
+      Rules.SetRule(sender.RuleType, value);
 
-    public void SelectMoveTime(float time)
-    {
-      Rules.Data.MoveTime = time;
-    }
+      foreach (RuleButton button in _ruleButtons
+        .Where(x => x.RuleType == sender.RuleType))
+      {
+        button.SetSprite(_inactiveButtonSprite);
+      }
 
-    public void SelectShapeFading(ShapeFadingType fading)
-    {
-      Rules.Data.ShapeFading = fading;
-    }
-
-    public void SelectFadingMoveCount(int count)
-    {
-      Rules.Data.FadingMoveCount = count;
+      sender.SetSprite(_activeButtonSprite);
     }
 
     private void SearchGame()
@@ -76,6 +92,7 @@ namespace CollectiveMind.TicTac3D.Runtime.Client.UI.LobbySettings
     {
       _searchGameButton.RemoveListener(SearchGame);
       _backButton.RemoveListener(CloseWindow);
+      _configLoader.UnloadConfig<GameConfig>();
     }
   }
 }

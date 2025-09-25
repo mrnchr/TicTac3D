db6a58bc2111ef9c903941b6fcefefc3c25fc2b8
Subbota Mikhail <mihail.subbota@inbox.ru>
2025-09-25 19:58:05 +0300

    build: Add tunnel launch to run script

--- yarun.cmd
+++ yarun.cmd
@@ -4,3 +4,5 @@ set buildFolder=%~2
 if "%buildFolder%"=="" set buildFolder=TicTac3D
 
 start npx @yandex-games/sdk-dev-proxy -p ".artifacts/Web/%buildFolder%" -i="%appId%" -с
+start cloudpub
+start https://tic-tac-3d.cloudpub.ru
